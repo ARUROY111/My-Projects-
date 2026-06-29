11ed0af25289b30b272cc0d97f125deeab502379
Arunava Roy <85797941+ARUROY111@users.noreply.github.com>
2026-06-29 23:26:14 +0530

    Create QuarantineFunction.py

---
+++ Self Healing Data Pipeline/Lambda/QuarantineFunction.py
@@ -0,0 +1,24 @@
+import boto3
+import json
+import os
+
+s3_client = boto3.client('s3')
+sqs_client = boto3.client('sqs')
+
+def lambda_handler(event, context):
+    bucket = event['bucket']
+    key = event['key']
+    error = event.get('error', 'Unknown Error')
+    new_key = key.replace('inbound/', 'quarantine/')
+
+    # Move to quarantine
+    s3_client.copy_object(Bucket=bucket, CopySource={'Bucket': bucket, 'Key': key}, Key=new_key)
+    s3_client.delete_object(Bucket=bucket, Key=key)
+
+    # Send alert to SQS
+    queue_url = os.environ['SQS_QUEUE_URL']
+    sqs_client.send_message(
+        QueueUrl=queue_url,
+        MessageBody=json.dumps({'bucket': bucket, 'bad_file': key, 'error': error})
+    )
+    return {"status": "quarantined"}
